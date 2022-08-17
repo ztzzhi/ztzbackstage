9d160800816831ac034ba5eb28cbb99ff8d3a696
ztzzhi <9972792+zhangtingzhi@user.noreply.gitee.com>
2022-08-17 14:41:31 +0800

    update

--- src/router/routerBefore.tsx
+++ src/router/routerBefore.tsx
@@ -6,9 +6,15 @@ import { allRouters } from "./index"
 export default function routerBefore({ children }: any) {
   const userPover = useSelector<RootState, any[]>(state => state.user.userPover)
   const location = useLocation()
+
+  const lastItem = location.pathname.split("/").pop()
+
   const pover = allRouters[0].children?.find(
     item => item.path === location.pathname
   )?.pover
+  if (!isNaN(Number(lastItem))) {
+    return <>{children}</>
+  }
   if (userPover?.length && userPover.some(item => item === pover)) {
     return <>{children}</>
   } else {
